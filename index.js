e67a33f6dc342fd443815a2eba1599e49b887fd7
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-05-14 23:16:59 +0300

    API Fetch Request
    The code block required for dynamic data extraction from the API has been added. Fetch Request and Map function were used.

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ const events = [
 ]
 
 new Calendar(events);
+//bu bölümde api tarafından dinamik veri alma işlemini yapabiliriz.
+// API JSON formatında veri döndürmelidir.
+// import Calendar from './calendar.js';
+
+// fetch('http://localhost:27312/api/ScheduleUser')
+//     .then(response => response.json())
+//     .then(events => {
+//         const eventArray = events.map(event => {
+//             return {
+//                 id: event.scheduleID,
+//                 eventName: event.description,
+//                 dateFrom: new Date(event.starttime).getTime(),
+//                 dateTo: new Date(event.endtime).getTime()
+//             };
+//         });
+
+//         new Calendar(eventArray);
+//     })
+//     .catch(error => {
+//         console.error('Error fetching events:', error);
+//     });
